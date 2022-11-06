2a109c7867c1f3dc3a9e99776d830138bce03475
AbdulAdal <k21007037@kcl.ac.uk>
2022-11-06 15:59:12 +0000

    Implement the primes 2

--- primes.py
+++ primes.py
@@ -3,4 +3,14 @@
 
 def primes(number_of_primes):
     list = []
+    if(number_of_primes==0 or number_of_primes < 0):
+        raise ValueError()
+    for num in range(2,101):
+        prime = True
+        for i in range(2,num):
+  
+            if (num%i==0):
+                prime = False
+        if prime and len(list)<number_of_primes:
+                list.append(num)
     return list
